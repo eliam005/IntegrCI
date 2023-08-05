c054027e8093c501dd863ebe18162a4b368e34ec
Eliam <salcov.ilya@yandex.ru>
2023-08-06 01:57:19 +0300

    test added

---
+++ src/test/java/ru/netology/javaqa/MaxServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+}
